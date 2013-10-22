0f47ba4a378dfdcfc85c1af539d44e5eab89744e
kmuruganandham <kmuruganandham@ebay.com>
2013-10-22 15:18:23 +0530

    HelloWorld spring app working with JBoss 7.0

---
+++ helloworld/src/com/mk/hw/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.mk.hw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController
+{
+	private String viewName = "hello";
+	private String key = "message";
+
+	@RequestMapping("/hello")
+	public ModelAndView hellowWorld()
+	{
+		String message = "Hello World, Spring 3.0!";
+		return new ModelAndView(viewName, key, message);
+	}
+}
