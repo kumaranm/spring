59397e8614096c69a3e8eb7d84c5f8b21cf26f7a
kmuruganandham <kmuruganandham@ebay.com>
2013-10-25 12:41:07 +0530

    base of contacts db application. only welcome page is shown

---
+++ mycontactsdb/WebContent/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>My Contact Database</title>
+</head>
+<body>
+    <a href="contactsdb.html">Goto Contacts Application</a>
+</body>
+</html>
---
+++ mycontactsdb/src/main/java/com/mk/cdb/dao/ContactDAOImpl.java
@@ -0,0 +1,34 @@
+package com.mk.cdb.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.mk.cdb.form.Contact;
+
+@Repository
+public class ContactDAOImpl implements ContactDAO
+{
+
+	@Override
+	public void addContact(Contact contact)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Contact> listContacts()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeContact(Integer Id)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
